0291fd3ecf78dedd89b6a717c1d4fd15a179f9ab
Somesh Singh <somesh.singh@nagarro.com>
2023-11-11 23:33:23 +0530

    Preparation for collab

--- skewing.py
+++ skewing.py
@@ -5,13 +5,13 @@ import numpy as np
 import easyocr
 
 results = {}
-reader = easyocr.Reader(['en'], gpu=False)
+reader = easyocr.Reader(['en'], gpu=True)
 
 
 coco_model = YOLO('yolov8n.pt')
 license_plate_detector = YOLO('./models/license_plate_detector.pt')
 
-cap = cv2.VideoCapture('./red old.mp4')
+cap = cv2.VideoCapture('./vid.avi')
 
 vehicles = [2, 3]
 
@@ -23,8 +23,8 @@ if (cap.isOpened()== False):
   print("Error opening video stream or file")
  
 
-pts1 = np.float32([[270,82], [940,0], [570,1023], [1080,678]])
-pts2 = np.float32([[0,0],[1400,0],[0,1400],[1400,1400]])
+# pts1 = np.float32([[270,82], [940,0], [570,1023], [1080,678]])
+# pts2 = np.float32([[0,0],[1000,0],[0,1000],[1000,1000]])
 
 count = 0
 
@@ -36,42 +36,43 @@ try:
         ret, frame = cap.read()
         if ret == True:
             results[frame_nmr] = {}
-            M = cv2.getPerspectiveTransform(pts1,pts2)
-            dst = cv2.warpPerspective(frame,M,(1400,1400))
-            # cv2.imshow('Output', dst)
+            # M = cv2.getPerspectiveTransform(pts1,pts2)
+            # dst = cv2.warpPerspective(frame,M,(1000,1000))
+            dst = frame
+            cv2.imshow('Output', dst)
 
-            detections = coco_model(dst)[0]
-            detections_ = []
-            for detection in detections.boxes.data.tolist():
-                x1, y1, x2, y2, score, class_id = detection
-                if int(class_id) in vehicles:
-                    detections_.append([x1, y1, x2, y2, score])
+            # detections = coco_model(dst)[0]
+            # detections_ = []
+            # for detection in detections.boxes.data.tolist():
+            #     x1, y1, x2, y2, score, class_id = detection
+            #     if int(class_id) in vehicles:
+            #         detections_.append([x1, y1, x2, y2, score])
             
-            license_plates = license_plate_detector(dst)[0]
+            # license_plates = license_plate_detector(dst)[0]
             
 
-            for license_plate in license_plates.boxes.data.tolist():
-                x1, y1, x2, y2, score, class_id = license_plate
+            # for license_plate in license_plates.boxes.data.tolist():
+            #     x1, y1, x2, y2, score, class_id = license_plate
 
-                if score*100 > 70:
+            #     if score*100 > 70:
                 
-                    license_plate_crop = dst[int(y1):int(y2), int(x1): int(x2), :]
+            #         license_plate_crop = dst[int(y1):int(y2), int(x1): int(x2), :]
 
-                    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]) 
+            #         kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]) 
 
-                    license_plate_crop = cv2.filter2D(license_plate_crop, -1, kernel)
+            #         license_plate_crop = cv2.filter2D(license_plate_crop, -1, kernel)
 
-                    license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+            #         license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
  
-                    cv2.imwrite('frame/frame{:d}.jpg'.format(count), license_plate_crop_gray)
+            #         cv2.imwrite('frame/frame{:d}.jpg'.format(count), license_plate_crop_gray)
                     
-                    detections = reader.readtext(license_plate_crop_gray)
+            #         detections = reader.readtext(license_plate_crop_gray)
                     
-                    for detection in detections:
-                        bbox, text, text_score = detection
-                        if (text_score*100 > 40):
+            #         for detection in detections:
+            #             bbox, text, text_score = detection
+            #             if (text_score*100 > 50):
                             
-                            write_csv_l([text, text_score])
+            #                 write_csv_l([text, text_score])
     
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
